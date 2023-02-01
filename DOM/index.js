08eaed5bf430d091feb4e9661a1f0348a9a4164d
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-01 20:04:48 +0100

    dom: add class huge to h1

--- DOM/index.js
+++ DOM/index.js
@@ -1,3 +1,4 @@
 document.querySelector('ul').lastElementChild.textContent = "Long live the DOM!";
 document.querySelector("li a").style.color = "red";
 document.querySelector(".btn").style.backgroundColor = "yellow";
+document.querySelector("h1").classList.add("huge");
