1b739873d579021cf6f8148880db61a463333bc4
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-01 19:59:54 +0100

    dom: change the bg color of the btn to yellow

--- DOM/index.js
+++ DOM/index.js
@@ -1,2 +1,3 @@
 document.querySelector('ul').lastElementChild.textContent = "Long live the DOM!";
 document.querySelector("li a").style.color = "red";
+document.querySelector(".btn").style.backgroundColor = "yellow";
