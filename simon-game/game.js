9b934372ceb694386e78442c468ca29ef461ac92
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-06 19:10:35 +0100

    simon: step 3 - adding click EL, animation and sounds

--- simon-game/game.js
+++ simon-game/game.js
@@ -8,3 +8,29 @@ function newSequence() {
 
     gamePattern.push(randomChosenColour);
 }
+
+// Game starts when any key is pressed
+$(document).keypress(function () {
+    newSequence();
+    console.log(gamePattern);
+});
+
+// Select the button
+$(".btn").click(function () {
+    let userChosenColour = $(this).attr("id");
+    console.log(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+// Flash the button
+function animatePress(currentColour) {
+    $("#" + currentColour).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+
+// Play the sound
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
