972f99150242b0e12628664686c7ec4396bb2a9f
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-11 19:30:21 +0100

    simon: step 5 - add level title increment

--- simon-game/game.js
+++ simon-game/game.js
@@ -2,18 +2,37 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 let userClickedPattern = [];
+let gameIsOn = false;
+let level = 0;
 
-function newSequence() {
+function nextSequence() {
+    // Pick a random number between 0 and 3
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
 
+    // Add the random colour to the game pattern
     gamePattern.push(randomChosenColour);
+
+    // Flash the button
+    flashButton(randomChosenColour);
+
+    // Play the sound
+    playSound(randomChosenColour);
+
+    // Increase the level
+    level++;
+    $("#level-title").text("Level " + level);
 }
 
 // Game starts when any key is pressed
 $(document).keypress(function () {
-    newSequence();
-    console.log(gamePattern);
+    if (!gameIsOn) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        gameIsOn = true;
+    }
+
+    console.log("attempt to restart a game")
 });
 
 // Select the button
@@ -21,14 +40,14 @@ $(".btn").click(function () {
     let userChosenColour = $(this).attr("id");
     console.log(userChosenColour);
     playSound(userChosenColour);
-    animatePress(userChosenColour);
+    flashButton(userChosenColour);
 
     userClickedPattern.push(userChosenColour);
     console.log(userClickedPattern);
 });
 
 // Flash the button
-function animatePress(currentColour) {
+function flashButton(currentColour) {
     // Add the pressed class
     $("#" + currentColour).addClass("pressed");
 
