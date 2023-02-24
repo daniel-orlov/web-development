d71bbf1f26e44696d6114de17f22ac2076be94f5
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-24 20:30:50 +0100

    newsletter: move static files to public dir

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -7,5 +7,10 @@ const request = require('request');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+// Handlers
+app.get('/', (req, res) => res.sendFile(__dirname + '/signup.html'));
 
 app.listen(port, () => console.log(`Server is listening on port ${port}!`));
