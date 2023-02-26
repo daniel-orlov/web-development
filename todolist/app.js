8b90bef2a5713b2e4dad77b11d5e2a2d0c6a6a14
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-26 19:15:05 +0100

    todolist: use function instead of a switch

--- todolist/app.js
+++ todolist/app.js
@@ -8,36 +8,7 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    const today = new Date();
-    const currentDay = today.getDay();
-
-    let day = '';
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday';
-            break;
-        case 1:
-            day = 'Monday';
-            break;
-        case 2:
-            day = 'Tuesday';
-            break;
-        case 3:
-            day = 'Wednesday';
-            break;
-        case 4:
-            day = 'Thursday';
-            break;
-        case 5:
-            day = 'Friday';
-            break;
-        case 6:
-            day = 'Saturday';
-            break;
-        default:
-            console.log('Error: current day is equal to: ' + currentDay);
-    }
-
+    const day = getDay();
     res.render('list', {dayOfWeek: day});
 });
 
