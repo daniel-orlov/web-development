9cc73d68481b2760f03e48059844cfde23034050
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-26 18:54:19 +0100

    todolist: install

--- todolist/app.js
+++ todolist/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const https = require("https");
 
 // Create a new express app
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
