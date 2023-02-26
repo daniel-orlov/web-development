3d4e5409bcf4085df2b708fcac8a96f7b67eae64
Daniel Orlov <daniel.orlov@protonmail.com>
2023-02-26 19:14:08 +0100

    todolist: response with day of the week

--- todolist/app.js
+++ todolist/app.js
@@ -5,13 +5,52 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    const today = new Date();
+    const currentDay = today.getDay();
+
+    let day = '';
+    switch (currentDay) {
+        case 0:
+            day = 'Sunday';
+            break;
+        case 1:
+            day = 'Monday';
+            break;
+        case 2:
+            day = 'Tuesday';
+            break;
+        case 3:
+            day = 'Wednesday';
+            break;
+        case 4:
+            day = 'Thursday';
+            break;
+        case 5:
+            day = 'Friday';
+            break;
+        case 6:
+            day = 'Saturday';
+            break;
+        default:
+            console.log('Error: current day is equal to: ' + currentDay);
+    }
+
+    res.render('list', {dayOfWeek: day});
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 });
+
+function getDay() {
+    const today = new Date();
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+    return today.toLocaleDateString('en-US', options);
+}
