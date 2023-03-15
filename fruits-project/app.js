a8136fde12505424389621425df50f60cc1b9b7b
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-15 19:24:33 +0100

    fruits: save a fruit

--- fruits-project/app.js
+++ fruits-project/app.js
@@ -6,3 +6,26 @@ mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+const fruitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please check your data entry, no name specified!'],
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10,
+    },
+    review: String,
+});
+
+const Fruit = mongoose.model('Fruit', fruitSchema);
+
+const fruit = new Fruit({
+    name: 'Apple',
+    rating: 7,
+    review: 'Pretty solid as a fruit.',
+});
+
+fruit.save().then(r => console.log("Response: " + r));
