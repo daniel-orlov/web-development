7466d71cf9bf067602ed0019e20a68afba638b83
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-15 19:36:45 +0100

    fruits: insert many and find all

--- fruits-project/app.js
+++ fruits-project/app.js
@@ -23,9 +23,49 @@ const fruitSchema = new mongoose.Schema({
 const Fruit = mongoose.model('Fruit', fruitSchema);
 
 const fruit = new Fruit({
-    name: 'Apple',
-    rating: 7,
-    review: 'Pretty solid as a fruit.',
+    name: 'Pineapple',
+    rating: 10,
+    review: '10 out of 10, would eat again',
 });
 
-fruit.save().then(r => console.log("Response: " + r));
+fruit.save().then(r => console.log("Saved a new fruit: " + r));
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    surname: String,
+    age: Number,
+    favouriteFruit: fruitSchema,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const person = new Person({
+    name: 'John',
+    surname: 'Doe',
+    age: 37,
+    favouriteFruit: fruit,
+});
+
+person.save().then(r => console.log("Saved a new person: " + r));
+
+const kiwi = new Fruit({
+    name: 'Kiwi',
+    rating: 8,
+    review: 'Cool fruit',
+});
+
+const orange = new Fruit({
+    name: 'Orange',
+    rating: 6,
+    review: 'Gets the job done',
+});
+
+const banana = new Fruit({
+    name: 'Banana',
+    rating: 9,
+    review: 'Great for smoothies',
+});
+
+Fruit.insertMany([kiwi, orange, banana]).then(r => console.log(r));
+
+Fruit.find().then(r => console.log(r));
