8572bab3ea0aa7031aa5f9492211fae61067f837
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-28 16:42:15 +0200

    react: add increment-decrement counter

--- 19-react-apps/src/App.jsx
+++ 19-react-apps/src/App.jsx
@@ -1,12 +1,17 @@
 import {Login} from "./components/Login";
+import {useState} from "react";
 
 let isLogged = false;
 
 export function App() {
+    const [count, setCount] = useState(0);
+
     return (
         <div className="container">
-            <h1>Hello</h1>
-            <Login />
+            {isLogged ? <h1>Welcome to the app</h1> : <Login/>}
+            <p>{count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
         </div>
     )
 }
