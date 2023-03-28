678f8b3d859bad5e8a4e24095b0f1bf4d7ada830
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-28 19:24:49 +0200

    react: add complex state

--- 19-react-apps/src/components/Login.jsx
+++ 19-react-apps/src/components/Login.jsx
@@ -6,6 +6,11 @@ export function Login() {
 
     const [buttonText, setButtonText] = useState(defaultButtonText);
 
+    const [fullName, setFullName] = useState({
+        fName: "",
+        lName: ""
+    })
+
     const handleMouseOver = () => {
         setButtonText("Go ahead, what are you waiting for?");
     }
@@ -14,13 +19,35 @@ export function Login() {
         setButtonText(defaultButtonText);
     }
 
+    const changeHandler = (event) => {
+        const {value, name} = event.target;
+
+        if (name === "fName") {
+            setFullName( prevValue => {
+                return {
+                    fName: value,
+                    lName: prevValue.lName
+                }
+            })
+        } else if (name === "lName") {
+            setFullName( prevValue => {
+                return {
+                    fName: prevValue.fName,
+                    lName: value,
+                }
+            })
+        }
+    }
+
     return (
-        <form className="form">
-            <Input type="text" placeholder="Username"/>
-            <Input type="password" placeholder="Password"/>
-            <button type="submit">Login</button>
+        <div>
+            <h1>Hello {fullName.fName} {fullName.lName}</h1>
+            <Input type="text" placeholder="First name" onChangeHanlder={changeHandler} value={fullName.fName}/>
+            <Input type="text" placeholder="Last name" onChangeHanlder={changeHandler} value={fullName.lName}/>
+            <Input type="password" placeholder="Password" onChangeHanlder={(event) => console.log(event.target.value)}/>
+            <button>Login</button>
             <br/>
             <button type="button" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>{buttonText}</button>
-        </form>
+        </div>
     )
 }
