5515f0619f61b621045545e9fda3fd0da724f9c5
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-28 17:28:46 +0200

    react: add event onClick

--- 19-react-apps/src/components/Login.jsx
+++ 19-react-apps/src/components/Login.jsx
@@ -1,11 +1,22 @@
 import {Input} from "./Input";
+import {useState} from "react";
 
 export function Login() {
+
+    const [formHeading, setFormHeading] = useState("Login");
+
+    const handleClick = () => {
+        setFormHeading("Welcome");
+        console.log("clicked");
+    }
+
     return (
         <form className="form">
+            <h1>{formHeading}</h1>
             <Input type="text" placeholder="Username"/>
             <Input type="password" placeholder="Password"/>
             <button type="submit">Login</button>
+            <button type="button" onClick={handleClick}>Click me</button>
         </form>
     )
 }
