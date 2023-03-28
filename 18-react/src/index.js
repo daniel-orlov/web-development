6eaae794b07dd53f8b488709486af9c66c171283
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-28 09:49:03 +0200

    react: more injection practice

--- 18-react/src/index.js
+++ 18-react/src/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const author = "Dan";
-const currentYear = 2023
-// For some reason the below code doesn't work. Will investigate later.
-// const currentYear = Date().getFullYear();
+const currentDate = Date();
+const currentYear = currentDate.getFullYear();
+
+console.log(currentYear);
 
 ReactDOM.render(
     <div>
         <p>Created by {author}.</p>
-        <p>© {currentYear}</p>
+        <p>© {currentDate.getFullYear()}</p>
     </div>,
     document.getElementById("root")
 );
