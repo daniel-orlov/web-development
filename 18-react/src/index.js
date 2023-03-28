d16186d7e29ae991cfe6ff6fb6e9e72a3988f296
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-28 10:38:26 +0200

    react: add dynamically changed style

--- 18-react/src/index.js
+++ 18-react/src/index.js
@@ -7,9 +7,27 @@ const customStyle = {
     border: "2px black solid"
 }
 
+const greetingStyle = {
+    color: "black"
+}
+
+const greet = function () {
+    const currentHours = new Date().getHours();
+    if (currentHours < 12) {
+        greetingStyle.color = "red";
+        return "Good morning!";
+    } else if (currentHours < 18) {
+        greetingStyle.color = "green"
+        return "Good afternoon!";
+    } else {
+        greetingStyle.color = "blue"
+        return "Good evening!";
+    }
+}
+
 ReactDOM.render(
     <div>
-        <h1 className="heading">Big letters here</h1>
+        <h1 className="heading" style={greetingStyle}>{greet()}</h1>
         <p contentEditable="true" spellCheck="false">Editable content on this line</p>
         <img className="rounded-square image" src="react.png" alt="react logo"/>
         <p style={customStyle}>Some bordered pink text that is hard to read</p>
