77be8d29fc01813836e7d24e07e3acd1c18d52f1
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-28 10:20:32 +0200

    react: add inline custom style

--- 18-react/src/index.js
+++ 18-react/src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+const customStyle = {
+    color: "pink",
+    border: "2px black solid"
+}
+
 ReactDOM.render(
     <div>
         <h1 className="heading">Big letters here</h1>
         <p contentEditable="true" spellCheck="false">Editable content on this line</p>
         <img className="rounded-square image" src="react.png" alt="react logo"/>
+        <p style={customStyle}>Some bordered pink text that is hard to read</p>
     </div>,
     document.getElementById("root")
 );
