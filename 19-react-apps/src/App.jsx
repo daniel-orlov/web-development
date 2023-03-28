992f173cbb12b1d695491a2dbf699e6c1e78f112
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-28 16:44:32 +0200

    react: add real-time clock

--- 19-react-apps/src/App.jsx
+++ 19-react-apps/src/App.jsx
@@ -6,12 +6,20 @@ let isLogged = false;
 export function App() {
     const [count, setCount] = useState(0);
 
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+    setInterval(() => {
+        setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
     return (
         <div className="container">
             {isLogged ? <h1>Welcome to the app</h1> : <Login/>}
             <p>{count}</p>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
+
+            <p>{time}</p>
         </div>
     )
 }
