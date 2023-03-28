51c9459b991e4119fd047c7bd0a70e2d591e7402
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-28 10:04:03 +0200

    react: add image and style it

--- 18-react/src/index.js
+++ 18-react/src/index.js
@@ -5,6 +5,7 @@ ReactDOM.render(
     <div>
         <h1 className="heading">Big letters here</h1>
         <p contentEditable="true" spellCheck="false">Editable content on this line</p>
+        <img className="rounded-square image" src="react.png" alt="react logo"/>
     </div>,
     document.getElementById("root")
 );
