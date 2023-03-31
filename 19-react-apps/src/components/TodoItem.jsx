6c4cc118eebccd3f3c0f0a315e5e626abffa2e7e
Daniel Orlov <daniel.orlov@protonmail.com>
2023-03-31 15:23:35 +0200

    react: add cross-out functionality

--- 19-react-apps/src/components/TodoItem.jsx
+++ 19-react-apps/src/components/TodoItem.jsx
@@ -1,5 +1,15 @@
+import {useState} from "react";
+
 export function TodoItem(props) {
+    const [isDone, setIsDone] = useState(false);
+
+    const crossOut = () => {
+        setIsDone(!isDone)
+    }
+
     return (
-        <li>{props.text}</li>
+        <div onClick={crossOut}>
+            <li style={{textDecoration: isDone ? "line-through" : null}} >{props.text}</li>
+        </div>
     )
 }
