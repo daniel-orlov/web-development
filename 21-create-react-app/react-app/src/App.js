172eb0c766cb264cb440302566f1e543d6a3bc48
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-02 19:11:48 +0200

    react-app: refactor to single state object

--- 21-create-react-app/react-app/src/App.js
+++ 21-create-react-app/react-app/src/App.js
@@ -2,24 +2,25 @@ import "./App.css";
 import {useEffect, useState} from "react";
 
 function App() {
-    const [frontendFramework, setFrontendFramework] = useState("React")
-    const [backendLanguage, setBackendLanguage] = useState("Node")
+    const [tech, setTech] = useState({
+        frontendFramework: "React", backendLanguage: "Node"
+    })
 
     useEffect(() => {
-        console.log("Frontend Framework: ", frontendFramework)
-        console.log("Backend Language: ", backendLanguage)
-    }, [frontendFramework, backendLanguage])
+        console.log("Frontend Framework: ", tech.frontendFramework)
+        console.log("Backend Language: ", tech.backendLanguage)
+    }, [tech])
 
     return (<div className="App">
-        <h1>Hello from {frontendFramework}</h1>
-        <button onClick={() => setFrontendFramework("React")}>React</button>
-        <button onClick={() => setFrontendFramework("Vue")}>Vue</button>
-        <button onClick={() => setFrontendFramework("Svelte")}>Svelte</button>
+        <h1>Hello from {tech.frontendFramework}</h1>
+        <button onClick={() => setTech({...tech, frontendFramework: "React"})}>React</button>
+        <button onClick={() => setTech({...tech, frontendFramework: "Vue"})}>Vue</button>
+        <button onClick={() => setTech({...tech, frontendFramework: "Angular"})}>Angular</button>
 
-        <h2>We are using {backendLanguage} in the Backend</h2>
-        <button onClick={() => setBackendLanguage("Node")}>Node</button>
-        <button onClick={() => setBackendLanguage("Go")}>Go</button>
-        <button onClick={() => setBackendLanguage("Python")}>Python</button>
+        <h2>We are using {tech.backendLanguage} in the Backend</h2>
+        <button onClick={() => setTech({...tech, backendLanguage: "Node"})}>Node</button>
+        <button onClick={() => setTech({...tech, backendLanguage: "Python"})}>Python</button>
+        <button onClick={() => setTech({...tech, backendLanguage: "Go"})}>Go</button>
     </div>);
 }
 
