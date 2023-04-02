3851234b08824b53ccaf565a389398eed4b94f70
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-02 19:44:52 +0200

    react-app: add color picker

--- 21-create-react-app/react-app/src/App.js
+++ 21-create-react-app/react-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import {useEffect, useReducer, useState} from "react";
+import {useEffect, useReducer, useRef, useState} from "react";
 
 function App() {
     const [tech, setTech] = useReducer((state, newState) => ({...state, ...newState}), {
@@ -17,7 +17,7 @@ function App() {
         console.log("Have Tried: ", tech.haveTried)
     }, [tech])
 
-    const submit = (e) => {
+    const submitStack = (e) => {
         e.preventDefault();
         setTech({frontendFramework: e.target[0].value, backendLanguage: e.target[1].value, haveTried: false})
         setFrontendFrameworks([...frontendFrameworks, {name: e.target[0].value, id: frontendFrameworks.length + 1}])
@@ -25,13 +25,23 @@ function App() {
     }
 
     const [backendLanguages, setBackendLanguages] = useState([{name: "Node", id: 1}, {
-        name: "Python",
-        id: 2
+        name: "Python", id: 2
     }, {name: "Go", id: 3}])
     const [frontendFrameworks, setFrontendFrameworks] = useState([{name: "React", id: 1}, {
-        name: "Vue",
-        id: 2
+        name: "Vue", id: 2
     }, {name: "Angular", id: 3}])
+
+    const colorTitle = useRef();
+    const colorHex = useRef();
+
+    const submitColor = (e) => {
+        e.preventDefault();
+        const title = colorTitle.current.value;
+        const hex = colorHex.current.value;
+        alert(`New Color: ${title} ${hex}`)
+        colorTitle.current.value = "";
+        colorHex.current.value = "#000000";
+    }
 
     return (<div className="App">
         <h1>Hello from {tech.frontendFramework}</h1>
@@ -51,14 +61,22 @@ function App() {
         <input type="checkbox" value={tech.haveTried} onChange={() => setTech({...tech, haveTried: !tech.haveTried})}/>
         <label>Yes</label>
 
-        <form onSubmit={submit}>
-            <h4>Not on the list? Add your own!</h4>
+        <h4>Not on the list? Add your own!</h4>
+        <form onSubmit={submitStack}>
             <input type="text" placeholder="Frontend Framework" value={tech.frontendFramework}
                    onChange={(e) => setTech({...tech, frontendFramework: e.target.value})}/>
             <input type="text" placeholder="Backend Language" value={tech.backendLanguage}
                    onChange={(e) => setTech({...tech, backendLanguage: e.target.value})}/>
             <button type="submit">Submit</button>
         </form>
+
+        <h4>Let's pick your favourite color!</h4>
+        <form onSubmit={submitColor}>
+            <input type="text" placeholder="Color Title" ref={colorTitle}/>
+            <input type="color" placeholder="Color Hex" ref={colorHex}/>
+            <button type="submit">Submit</button>
+        </form>
+
     </div>);
 }
 
