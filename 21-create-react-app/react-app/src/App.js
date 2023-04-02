c48508751de2093ed01b0c8eb055e960ea07c61f
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-02 19:02:16 +0200

    react-app: use state and buttons to change it

--- 21-create-react-app/react-app/src/App.js
+++ 21-create-react-app/react-app/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
+import {useState} from "react";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Hello from React App</h1>
-        </div>
-    );
+    const [tech, setTech] = useState("React")
+
+    return (<div className="App">
+        <h1>Hello from {tech}</h1>
+        <button onClick={() => setTech("React")}>React</button>
+        <button onClick={() => setTech("Vue")}>Vue</button>
+        <button onClick={() => setTech("Svelte")}>Svelte</button>
+    </div>);
 }
 
 export default App;
