ba63210f4e34bc0044b1a313408ce25396e71254
Daniel Orlov <daniel.orlov@protonmail.com>
2023-04-02 18:35:36 +0200

    react: fix key error in the dish list

--- 20-react-more/src/components/Main/Main.jsx
+++ 20-react-more/src/components/Main/Main.jsx
@@ -1,7 +1,8 @@
 export function Main(props) {
     return (<>
         <section>
-            Here we cook:
+            <img height={200} src="https://source.unsplash.com/1600x900/?food" alt="Food"/>
+            <h2>Our Menu</h2>
             <ul>
                 {props.dishes.map(dish => <li key={dish.id}>{dish.title}</li>)}
             </ul>
